9ecd3aa12a8687f0cf54c7573070ac6154cde012
huyhuynh <huynhlonghuy.work@gmail.com>
2024-04-23 14:48:03 -0700

    Adding option feature

--- src/View/GameFrame.java
+++ src/View/GameFrame.java
@@ -1,17 +1,56 @@
 package src.View;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GameFrame {
-    private final JFrame frame;
-
+public class GameFrame implements ActionListener {
+    private JFrame myFrame;
+    private JMenuBar myOptionBar;
+    private JMenu mySetting, myHelp;
+    private JMenuItem myStart, myReset, myExit;
 
     public GameFrame() {
-        this.frame = new JFrame("Trivia Maze");
-        frame.setSize(650, 500);
-        frame.setLocationRelativeTo(null);
-        frame.setLayout(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        initializeFrame();
+        initializeOptionBar();
     }
-}
+
+    private void initializeFrame() {
+        this.myFrame = new JFrame();
+        myFrame.setSize(650, 500);
+        myFrame.setLocationRelativeTo(null);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setLayout(null);
+        myFrame.setVisible(true);
+    }
+    private void initializeOptionBar() {
+        this.myOptionBar = new JMenuBar();
+
+        // Setting option
+        this.mySetting = new JMenu("Setting");
+
+        this.myStart = new JMenuItem("Start");
+        this.myReset = new JMenuItem("Rest");
+        this.myExit = new JMenuItem("Exit");
+
+        myExit.addActionListener(this);
+        myReset.addActionListener(this);
+        myStart.addActionListener(this);
+
+        this.mySetting.add(myStart);
+        this.mySetting.add(myReset);
+        this.mySetting.add(myExit);
+
+
+        this.myOptionBar.add(mySetting);
+
+        this.myFrame.setJMenuBar(myOptionBar);
+        // Help option
+
+    }
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == myExit) {
+            myFrame.dispose();
+        }
+    }
+ }
