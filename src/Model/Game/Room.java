28a69057fbb8117ae21b01d064d4cea275dcafe2
Drewwb <drew_brown@frontier.com>
2024-05-27 13:38:11 -0700

    hardcoded room directions

--- src/Model/Game/Room.java
+++ src/Model/Game/Room.java
@@ -4,27 +4,102 @@ public class Room {
     private Door[] doors;
     private boolean visited;
     private int myRoomNumber;
+    final private int NORTH = 0;
+    final private int EAST = 1;
+    final private int SOUTH = 2;
+    final private int WEST = 3;
+
 
     public Room(int roomNumber) {
         this.visited = false;
         this.myRoomNumber = roomNumber;
 
-        if (roomNumber >= 10) {
-            if (roomNumber == 10 || roomNumber == 14 || roomNumber == 20 || roomNumber == 24) {
+        if(roomNumber >= 10) {
+            if(roomNumber == 10) {
                 this.doors = new Door[2];
-
-            } else if (roomNumber == 11 || roomNumber == 12 || roomNumber == 13 || roomNumber == 21 || roomNumber == 22 || roomNumber == 23) {
+                Direction direction = Direction.values()[EAST];
+                Direction direction2 = Direction.values()[SOUTH];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+            } else if(roomNumber == 11 || roomNumber == 12 || roomNumber == 13) {
                 this.doors = new Door[3];
-
-            } else {
+                Direction direction = Direction.values()[EAST];
+                Direction direction2 = Direction.values()[SOUTH];
+                Direction direction3 = Direction.values()[WEST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+                doors[2] = new Door(direction3);
+            } else if(roomNumber == 14) {
+                this.doors = new Door[2];
+                Direction direction = Direction.values()[SOUTH];
+                Direction direction2 = Direction.values()[WEST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+            } else if (roomNumber == 15) {
+                this.doors = new Door[3];
+                Direction direction = Direction.values()[NORTH];
+                Direction direction2 = Direction.values()[EAST];
+                Direction direction3 = Direction.values()[SOUTH];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+                doors[2] = new Door(direction3);
+            } else if (roomNumber == 16 || roomNumber == 17 || roomNumber == 18) {
                 this.doors = new Door[4];
+                Direction direction = Direction.values()[NORTH];
+                Direction direction2 = Direction.values()[EAST];
+                Direction direction3 = Direction.values()[SOUTH];
+                Direction direction4 = Direction.values()[WEST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+                doors[2] = new Door(direction3);
+                doors[3] = new Door(direction4);
+            } else if (roomNumber == 19) {
+                this.doors = new Door[3];
+                Direction direction = Direction.values()[NORTH];
+                Direction direction2 = Direction.values()[SOUTH];
+                Direction direction3 = Direction.values()[WEST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+                doors[2] = new Door(direction3);
+            } else if (roomNumber == 20) {
+                this.doors = new Door[2];
+                Direction direction = Direction.values()[NORTH];
+                Direction direction2 = Direction.values()[EAST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+            } else if (roomNumber == 21 || roomNumber == 22 || roomNumber == 23) {
+                this.doors = new Door[3];
+                Direction direction = Direction.values()[NORTH];
+                Direction direction2 = Direction.values()[EAST];
+                Direction direction3 = Direction.values()[WEST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
+                doors[2] = new Door(direction3);
+            } else if (roomNumber == 24) {
+                this.doors = new Door[2];
+                Direction direction = Direction.values()[NORTH];
+                Direction direction2 = Direction.values()[WEST];
+                doors[0] = new Door(direction);
+                doors[1] = new Door(direction2);
             }
-        } else {
-            // Default room with 4 doors
-            this.doors = new Door[4];
         }
 
-        initializeDoors();
+//        if (roomNumber >= 10) {
+//            if (roomNumber == 10 || roomNumber == 14 || roomNumber == 20 || roomNumber == 24) {
+//                this.doors = new Door[2];
+//
+//            } else if (roomNumber == 11 || roomNumber == 12 || roomNumber == 13 || roomNumber == 21 || roomNumber == 22 || roomNumber == 23) {
+//                this.doors = new Door[3];
+//
+//            } else {
+//                this.doors = new Door[4];
+//            }
+//        } else {
+//            // Default room with 4 doors
+//            this.doors = new Door[4];
+//        }
+//
+//        initializeDoors();
     }
 
     private void initializeDoors() {
